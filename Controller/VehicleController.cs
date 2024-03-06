8299b5966f6984e2fd659f36a93756e9e6f45626
sgundluru <sgundluru@usf.edu>
2024-03-06 15:59:02 -0500

    First Push of the Project

---
+++ Controller/VehicleController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleConsoleApp.Controller
+{
+    public class VehicleController
+    {
+        private IVehicle vehicle;
+
+        public VehicleController(IVehicle vehicle)
+        {
+            this.vehicle = vehicle;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter your choice: ");
+                Console.WriteLine("1. Start");
+                Console.WriteLine("2. Stop");
+                Console.WriteLine("3. Accelerate");
+                Console.WriteLine("4. Exit");
+
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        vehicle.Start();
+                        break;
+                    case 2:
+                        vehicle.Stop();
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter acceleration amount: ");
+                        int accelerationAmount = int.Parse(Console.ReadLine());
+                        vehicle.Accelerate(accelerationAmount);
+                        break;
+                    case 4:
+                        Console.WriteLine("Exiting the program.");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+            }
+        }
+    }
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VehicleConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
