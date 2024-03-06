fcd311f3b833d5164ad8e6a95b4d1d6b91a57055
sgundluru <sgundluru@usf.edu>
2024-03-06 16:06:42 -0500

    Added More Changes

--- Controller/VehicleController.cs
+++ Controller/VehicleController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VehicleConsoleApp.Interface;
 
 namespace VehicleConsoleApp.Controller
 {
--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            IVehicle vehicle = new Vehicle();
+            VehicleController vehicleController = new VehicleController(vehicle);
+            vehicleController.Run();
         }
     }
 }
